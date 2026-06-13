dfda435db904f38696bd67b1ecbd96cc5e300a09
Dheeraj Panyam <dheeraj.panyam@gmail.com>
2026-06-14 03:09:27 +0530

    Add patterns.py with various pattern printing examples

---
+++ 04_ques_from_1_2_3/patterns.py
@@ -0,0 +1,79 @@
+n = int(input("enter n = "))
+
+
+# for i in range(n):
+#     for j in range(n):
+#         print("*",end="")
+#     print()
+
+
+# i=1
+# while n>=i:
+#     j=1
+#     while i>=j:
+#         print(f"{j}" , end="")
+#         j=j+1
+#     print()
+#     i=i+1
+
+
+# i=1
+# while n>=i:
+#     j=1
+#     while i>=j:
+#         print(f"*" , end="")
+#         j=j+1
+#     print()
+#     i=i+1
+
+
+# i=1
+# j=0
+
+# while i <=n:
+#     j=1
+#     for j in range(i):
+#         print(f"{i}",end="")
+#     print()
+#     i=i+1 
+
+
+
+# for i in range(n):
+#     for j in range(n-i):
+#         print("*",end="")
+#     print()
+
+
+# for i in range(n):
+#     for j in range(n-i):
+#         print(f"{j+1}",end="")
+#     print()
+
+
+
+# for i in range(n):
+#     j=0
+#     for j in range(n-i-1):
+#         print(" ",end="")
+#     j=0
+#     for j in range(i*2+1):
+#         print("*",end="")
+#     j=0
+#     for j in range(n-i-1):
+#         print(" ",end="")
+#     print("\n") 
+    
+
+# for i in range(n):
+#     j=0
+#     for j in range(i):
+#         print(" ",end="")
+#     j=0
+#     for j in range((2*n)-(2*i+1)):
+#         print("*",end="")
+#     j=0
+#     for j in range(i):
+#         print(" ",end="")
+#     print("\n") 
+    
